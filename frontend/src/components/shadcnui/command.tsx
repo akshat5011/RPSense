2a19ee9cab12d73e22d686b39cd347b718ed66ff
akshat5011 <akshatgarg2k4@gmail.com>
2025-07-11 09:11:43 +0530

    renamed ui -> shadcn ui

--- frontend/src/components/shadcnui/command.tsx
+++ frontend/src/components/shadcnui/command.tsx
@@ -11,7 +11,7 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog"
+} from "@/components/shadcnui/dialog"
 
 function Command({
   className,
--- frontend/src/components/shadcnui/sidebar.tsx
+++ frontend/src/components/shadcnui/sidebar.tsx
@@ -7,23 +7,23 @@ import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/shadcnui/button"
+import { Input } from "@/components/shadcnui/input"
+import { Separator } from "@/components/shadcnui/separator"
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
-} from "@/components/ui/sheet"
-import { Skeleton } from "@/components/ui/skeleton"
+} from "@/components/shadcnui/sheet"
+import { Skeleton } from "@/components/shadcnui/skeleton"
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/components/shadcnui/tooltip"
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
